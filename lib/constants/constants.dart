31f15ea25060effc3f9b8debe795a6ff4992ed0e
Harshvardhan Kadam <harshvardhankadam89@gmail.com>
2022-01-04 00:25:13 +0530

    common code

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final kLabelStyle = GoogleFonts.firaSans(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+);

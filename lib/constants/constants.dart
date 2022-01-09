7161bf4d44d4f055e241f54964b4367e0085961e
Harshvardhan Kadam <harshvardhankadam89@gmail.com>
2022-01-09 19:13:39 +0530

    const decoration

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,5 +3,21 @@ import 'package:google_fonts/google_fonts.dart';
 
 final kLabelStyle = GoogleFonts.firaSans(
   color: Colors.white,
-  fontWeight: FontWeight.bold,
+  fontSize: 20.0,
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Colors.purple[200],
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 5.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white,
 );

eff5147f2e9e94e403b35db5ac0a11fd370fc4ec
Harshvardhan Kadam <harshvardhankadam89@gmail.com>
2022-01-20 00:26:12 +0530

    added external urls for signin

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -268,7 +268,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                         _buildSocialBtn(
                           () async {
-                            const mailurl = 'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin';
+                            const mailurl = 'https://accounts.google.com/signin';
                             if (await canLaunch(mailurl)) launch(mailurl);
                           },
                           AssetImage(
